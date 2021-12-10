5ad3c1ed813818290214b7d51258bcc64982b6a7
anonymous anonymous <tammy.su@kellogg.northwestern.edu>
2021-12-10 15:32:04 +0000

    Display only signed in users categories on index

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,7 +5,7 @@ class CategoriesController < ApplicationController
 
   # GET /categories
   def index
-    @categories = Category.all
+    @categories = current_chef.categories.page(params[:page]).per(10)
   end
 
   # GET /categories/1
