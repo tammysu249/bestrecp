ed65b6c34698cd9a560c680bdacebd8259f19cb3
anonymous anonymous <tammy.su@kellogg.northwestern.edu>
2021-12-10 15:31:15 +0000

    Generate chefs resource with devise

---
+++ app/controllers/chefs_controller.rb
@@ -0,0 +1,58 @@
+class ChefsController < ApplicationController
+  before_action :set_chef, only: [:show, :edit, :update, :destroy]
+
+  # GET /chefs
+  def index
+    @chefs = Chef.all
+  end
+
+  # GET /chefs/1
+  def show
+  end
+
+  # GET /chefs/new
+  def new
+    @chef = Chef.new
+  end
+
+  # GET /chefs/1/edit
+  def edit
+  end
+
+  # POST /chefs
+  def create
+    @chef = Chef.new(chef_params)
+
+    if @chef.save
+      redirect_to @chef, notice: 'Chef was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  # PATCH/PUT /chefs/1
+  def update
+    if @chef.update(chef_params)
+      redirect_to @chef, notice: 'Chef was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  # DELETE /chefs/1
+  def destroy
+    @chef.destroy
+    redirect_to chefs_url, notice: 'Chef was successfully destroyed.'
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_chef
+      @chef = Chef.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def chef_params
+      params.require(:chef).permit(:first_name, :last_name)
+    end
+end
---
+++ app/views/chefs/_chef.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! chef, :id, :first_name, :last_name, :created_at, :updated_at
+json.url chef_url(chef, format: :json)
