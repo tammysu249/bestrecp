9212742358d83c38ac4125861ff1b59e6f302b55
anonymous anonymous <tammy.su@kellogg.northwestern.edu>
2021-12-10 15:32:20 +0000

    Generate graphiti resources for categories

---
+++ app/resources/category_resource.rb
@@ -0,0 +1,12 @@
+class CategoryResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :category_name, :string
+  attribute :chef_id, :integer
+
+  # Direct associations
+
+  # Indirect associations
+
+end
